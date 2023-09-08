3425e8cbe4781cdc3183cc87af22b1eaf7ca6199
ArthurAlg <aliagaarturo2004@gmail.com@>
2023-09-08 18:09:30 -0500

    Nuevo proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoPersonal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
